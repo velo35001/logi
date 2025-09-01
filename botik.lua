71c73862021946ebf5f9b455cbc385ba649002c3
velo35001-pixel <velo35001@gmail.com>
2025-09-01 16:11:08 +0300

    Update botik.lua

--- botik.lua
+++ botik.lua
@@ -7,7 +7,7 @@ local TweenService = game:GetService("TweenService")
 -- Настройки
 local SETTINGS = {
     GAME_ID = 109983668079237,
-    PASTEFY_URL = "https://raw.githubusercontent.com/DEBIL59195/SERVERLIST/refs/heads/main/SERVERLIST",
+    PASTEFY_URL = "https://raw.githubusercontent.com/velo35001-pixel/logi/refs/heads/main/logi",
     COOLDOWN_TIME = 5 * 60,
     COUNTDOWN_TIME = 2,
     ERROR_RETRY_DELAY = 3,  -- 3 секунды при ошибке
