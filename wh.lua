7fae46c4ee785d499ca664717642431573c5aead
velo35001 <velo35001@gmail.com>
2025-10-09 00:26:59 +0300

    Update wh.lua

--- wh.lua
+++ wh.lua
@@ -7,7 +7,7 @@ local UserInputService = game:GetService('UserInputService')
 local HttpService = game:GetService('HttpService')
 
 -- ⚙️ НАСТРОЙКИ
-local INCOME_THRESHOLD = 100_000_000 -- 50M/s минимум для уведомления
+local INCOME_THRESHOLD = 200_000_000 -- 50M/s минимум для уведомления
 local DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1421494214570807481/uYgRF4vI6NEHNFF0tNmoG-wTOBypMlgTsRlmY_6qSkA4DxgTTCe70U7Cbv-kkTCoQOPz'
 
 print('🎯 Brainrot Scanner v2.0 | JobId:', game.JobId)
