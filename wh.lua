a7c3dc570f8ace175eacacfcbd304b4a688d8f00
velo35001 <velo35001@gmail.com>
2025-10-23 15:34:37 +0300

    Update wh.lua

--- wh.lua
+++ wh.lua
@@ -11,7 +11,7 @@ local INCOME_THRESHOLD = 10_000_000 -- 50M/s минимум для уведом�
 local HIGH_PRIORITY_THRESHOLD = 500_000_000 -- 500M/s для особо важных объектов
 local MEDIUM_PRIORITY_THRESHOLD = 100_000_000 -- 100M/s для среднего приоритета
 local DISCORD_WEBHOOK_URL =
-    'https://ptb.discord.com/api/webhooks/1430892859019497634/jmvY70FvYmQOpYzr_oGSdXGEKYgt4YVKJDiS0f7qRKTE-H1U3uD7bqPqR8R1-TzTfekv\'
+    'https://ptb.discord.com/api/webhooks/1426282608710647952/bmfmWPMug07ht7nRa_QeCVi7tfItybezKVkZ2tmw7lsODttiUnSnYJArl6UchxqIbeyT'
 local MEDIUM_WEBHOOK_URL =
     'https://discord.com/api/webhooks/1421494214570807481/uYgRF4vI6NEHNFF0tNmoG-wTOBypMlgTsRlmY_6qSkA4DxgTTCe70U7Cbv-kkTCoQOPz'
 
