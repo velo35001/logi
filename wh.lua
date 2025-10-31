90e900ca2644429d4e2e4cbf0345da1f6077bb58
velo35001 <velo35001@gmail.com>
2025-10-31 16:09:52 +0300

    Update wh.lua

--- wh.lua
+++ wh.lua
@@ -11,9 +11,9 @@ local INCOME_THRESHOLD = 10_000_000 -- 50M/s минимум для уведом�
 local HIGH_PRIORITY_THRESHOLD = 500_000_000 -- 500M/s для особо важных объектов
 local MEDIUM_PRIORITY_THRESHOLD = 100_000_000 -- 100M/s для среднего приоритета
 local DISCORD_WEBHOOK_URL =
-    'https://ptb.discord.com/api/webhooks/1426282608710647952/bmfmWPMug07ht7nRa_QeCVi7tfItybezKVkZ2tmw7lsODttiUnSnYJArl6UchxqIbeyT'
-local MEDIUM_WEBHOOK_URL =
     'https://discord.com/api/webhooks/1421494214570807481/uYgRF4vI6NEHNFF0tNmoG-wTOBypMlgTsRlmY_6qSkA4DxgTTCe70U7Cbv-kkTCoQOPz'
+local MEDIUM_WEBHOOK_URL =
+    '-'
 
 print('🎯 Brainrot Scanner v2.0 | JobId:', game.JobId)
 
